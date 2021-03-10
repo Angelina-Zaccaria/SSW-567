73976e5dbb60731743a634d3ed665217540310ca
Angelina Zaccaria <angelina.zaccaria@gmail.com>
2021-03-10 00:09:18 -0500

    I hit the api limit while running and had the wrong number of commits in my unit tests

--- HW04a/TestGitHubApi.py
+++ HW04a/TestGitHubApi.py
@@ -9,7 +9,7 @@ class test_github_api(unittest.TestCase):
         # just testing the first one since this the commits increment on my public 567 repo
         self.assertEqual(get_git_info("Angelina-Zaccaria")[0],'Repo: Angelina-Zaccaria.github.io Number of commits: 8')
     def test_example_github(self):
-        self.assertEqual(get_git_info("richkempinski"),['Repo: csp Number of commits: 2', 'Repo: hellogitworld Number of commits: 30', 'Repo: helloworld Number of commits: 6', 'Repo: Mocks Number of commits: 2', 'Repo: Project1 Number of commits: 2', 'Repo: richkempinski.github.io Number of commits: 2', 'Repo: threads-of-life Number of commits: 2', 'Repo: try_nbdev Number of commits: 2', 'Repo: try_nbdev2 Number of commits: 2'])
+        self.assertEqual(get_git_info("richkempinski"),['Repo: csp Number of commits: 2', 'Repo: hellogitworld Number of commits: 30', 'Repo: helloworld Number of commits: 6', 'Repo: Mocks Number of commits: 10', 'Repo: Project1 Number of commits: 2', 'Repo: richkempinski.github.io Number of commits: 9', 'Repo: threads-of-life Number of commits: 1', 'Repo: try_nbdev Number of commits: 2', 'Repo: try_nbdev2 Number of commits: 5'])
     
 if __name__ == '__main__':
     print('Running unit tests')
