0a574bd5339effd82a61cfd184c2bb2069c991f9
Angelina Zaccaria <angelina.zaccaria@gmail.com>
2021-03-10 00:00:15 -0500

    using the number of commits on a repo you're currently in for a unit test is not a smart thing to do

--- HW04a/TestGitHubApi.py
+++ HW04a/TestGitHubApi.py
@@ -6,7 +6,8 @@ class test_github_api(unittest.TestCase):
     def test_invalidInput(self):
         self.assertEqual(get_git_info("Angelina-Zaccari"),"User not found")
     def test_my_github(self):
-        self.assertEqual(get_git_info("Angelina-Zaccaria"),['Repo: Angelina-Zaccaria.github.io Number of commits: 8', 'Repo: BeeEat Number of commits: 30', 'Repo: ButterflyBot Number of commits: 30', 'Repo: Complexity Number of commits: 30', 'Repo: linux-4.9-azaccari2 Number of commits: 4', 'Repo: osddemo Number of commits: 1', 'Repo: sds1910 Number of commits: 23', 'Repo: SSW-567 Number of commits: 14'])
+        # just testing the first one since this the commits increment on my public 567 repo
+        self.assertEqual(get_git_info("Angelina-Zaccaria")[0],'Repo: Angelina-Zaccaria.github.io Number of commits: 8')
     def test_example_github(self):
         self.assertEqual(get_git_info("richkempinski"),['Repo: csp Number of commits: 2', 'Repo: hellogitworld Number of commits: 30', 'Repo: helloworld Number of commits: 6', 'Repo: Mocks Number of commits: 2', 'Repo: Project1 Number of commits: 2', 'Repo: richkempinski.github.io Number of commits: 2', 'Repo: threads-of-life Number of commits: 2', 'Repo: try_nbdev Number of commits: 2', 'Repo: try_nbdev2 Number of commits: 2'])
     
